ad5658eaa175eb24c6e3d21ac910d9cd61927404
Mehwish <39596277+mehwishh247@users.noreply.github.com>
2025-06-29 16:48:36 +0500

    Updated sensor route with DELETE

--- src/smart_fleet/app/routes/sensor.py
+++ src/smart_fleet/app/routes/sensor.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from smart_fleet.app.schemas.sensor import Sensor, SensorResponse
 
 sensor_route = APIRouter()
@@ -6,20 +7,38 @@ sensor_route = APIRouter()
 sensor_list = []
 sensor_id = 1
 
+def find_sensor_by_id(sensor_id):
+    for sensor in sensor_list:
+        if sensor['sensor_id'] == sensor_id:
+            return sensor
+
+@sensor_route.get('/sensors/{sensor_id}', response_model=SensorResponse)
+def find_vehicle(sensor_id: int):
+    sensor = find_sensor_by_id(sensor_id)
+    if sensor:
+        return sensor
+        
+    raise HTTPException(404, detail='Sensor not found in the list!')
+
 @sensor_route.post('/sensors', response_model=SensorResponse)
 def create_sensor(sensor: Sensor):
     global sensor_id
-    new_sensor = {'id': sensor_id, **sensor.dict()}
+    new_sensor = {'sensor_id': sensor_id, **sensor.dict()}
 
     sensor_id += 1
     sensor_list.append(new_sensor)
     
     return new_sensor
 
-@sensor_route.get('/sensors/{sensor_id}', response_model=SensorResponse)
-def find_vehicle(sensor_id: int):
-    for sensor in sensor_list:
-        if sensor['sensor_id'] == sensor_id:
-            return sensor
-        
-    raise HTTPException(404, detail='Sensor not found in the list!')
+@sensor_route.delete('/sensor/{sensor_id}', status_code=200)
+def delete_sensor(sensor_id: int):
+    sensor = find_sensor_by_id(sensor_id)
+    if sensor:
+        sensor_list.remove(sensor)
+        print(f"Deleted sensor {sensor_id}")
+        return JSONResponse(
+            content={
+            "message": "Sensor removed successfully!",
+            "sensor": sensor}
+        )
+    raise HTTPException(status_code=404, detail='No sensor found with given ID')
