bccc44a98fc30196a21207981db180f60a3ed7eb
Mehwish <39596277+mehwishh247@users.noreply.github.com>
2025-07-16 16:47:04 +0500

    Method to return app engine

--- src/smart_fleet/app/core/database.py
+++ src/smart_fleet/app/core/database.py
@@ -1,9 +1,14 @@
 from sqlmodel import SQLModel, create_engine
 
-def app_engine(db_name):
+def create_app_engine(db_name):
     url = f"sqlite:///{db_name}.db"
-    return create_engine(url, echo=True)
+    global engine
+    engine = create_engine(url, echo=True)
+    return engine
+
+def get_engine():
+    return engine
 
 def create_db_and_tables(db_name: str):
-    engine = app_engine(db_name=db_name)
+    engine = create_app_engine(db_name=db_name)
     SQLModel.metadata.create_all(engine)
