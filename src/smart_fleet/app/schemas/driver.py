934fce522debb5d7edece06515b78381eb4e67fc
Mehwish <39596277+mehwishh247@users.noreply.github.com>
2025-08-11 22:31:43 +0500

    Upgraded driver's schema with more fields

--- src/smart_fleet/app/schemas/driver.py
+++ src/smart_fleet/app/schemas/driver.py
@@ -1,12 +1,26 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
+from smart_fleet.app.schemas.enums import DriverStatus
 
-class Driver(BaseModel):
+class DriverBase(BaseModel):
     name: str
-    license_number: str
-    status: str
+    license_number: str = Field(..., min_length=5, max_length=10)
     phone: Optional[str] = None
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
+    status: DriverStatus
+
+class DriverCreate(DriverBase):
+    pass
+
+class DriverUpdate(BaseModel):
+    name: Optional[str] = None
+    license_number: Optional[str] = Field(None, min_length=5, max_length=10)
+    phone: Optional[str] = None
+    email: Optional[EmailStr] = None
+    status: Optional[DriverStatus] = None
+
+class DriverResponse(DriverBase):
+    driver_id: int
     class Config:
         orm_mode = True
     
