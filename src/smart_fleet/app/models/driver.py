33828c306ad89fbb701535379627a81de5c2cc55
Mehwish <39596277+mehwishh247@users.noreply.github.com>
2025-08-11 22:33:54 +0500

    Setup DB model for driver entity

---
+++ src/smart_fleet/app/models/driver.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class DriverBase(SQLModel):
+    name: str
+    license_number: str = Field(..., min_length=5, max_length=10, unique=True)
+    phone: Optional[str] = None
+    email: str = Field(..., unique=True, index=True)
+    status: str
+
+class Driver(DriverBase, table=True):
+    driver_id: int = Field(default=None, primary_key=True, index=True)
+
+class DriverCreate(DriverBase):
+    pass
+
+class DriverRead(DriverBase):
+    driver_id: int
